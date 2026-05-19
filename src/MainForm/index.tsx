66695c49d29d459869d30671c4efb5c367a3e106
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-05-19 11:12:44 -0300

    Refazendo o MainForm

--- src/MainForm/index.tsx
+++ src/MainForm/index.tsx
@@ -1,121 +1,110 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import styles from './styles.module.css';
-import { useState, useEffect } from 'react';
 import { Container } from '../components/Container';
 import { InputPadrao } from '../components/inputPadrao';
 import { BotaoPadrao } from '../components/BotaoPadrao';
+import { useState, useEffect } from 'react';
+import styles from './styles.module.css'
 
-interface DadosCurso {
-    nomeCurso: string;
+interface DadosCurso{
+    nomecurso: string;
     periodo: string;
-}
-interface MainFormProps {
-    aoAdicionar: (curso: any) => void;
-    aoAtualizar: (curso: any) => void;
-    cursoEmEdicao: any | null;
-
-
+} 
+interface MainFormProps{
+    aoAdicionar: (curso:any) => void;
+    aoAtualizar: (curso:any) => void;
+    cursoEmEdicao: (any|null);
 }
 
-export function MainForm({ aoAdicionar, aoAtualizar, cursoEmEdicao }
-    : MainFormProps) {
-    const [DadosCurso, setDadosCurso] = useState<DadosCurso>({
-        nomeCurso: '',
-        periodo: '',
+export function MainForm({aoAdicionar, aoAtualizar, cursoEmEdicao}:MainFormProps){
 
-    })
-    useEffect(() => {
-        if (cursoEmEdicao) {
-            setDadosCurso({
-                nomeCurso: cursoEmEdicao.nome,
+    const [DadosCurso, setDadosCurso] = useState<DadosCurso>({nomecurso : '', periodo :''});
+    useEffect(()=>{
+        if(cursoEmEdicao) {
+                setDadosCurso({
+                nomecurso: cursoEmEdicao.nome,
                 periodo: cursoEmEdicao.periodo
             });
-        } else {
-            setDadosCurso({ nomeCurso: '', periodo: '' });
-        }
-    }, [cursoEmEdicao])
-
-    const lidarComMudanca = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        }else{
+                setDadosCurso({
+                    nomecurso : "",
+                    periodo : ""
+                });
+            }
+    },[cursoEmEdicao]);
+    const lidarComMudanca = (e:React.ChangeEvent<HTMLInputElement|HTMLSelectElement>)=>{
         setDadosCurso({
-            ...DadosCurso,
-            [e.target.name]: e.target.value,
-
+            ...DadosCurso, 
+            [e.target.name]: e.target.value
         });
-    };
+    }
 
-    const cadastrarCurso = (e: any) => {
+
+    const cadastrarCurso = (e:any)=>{
         e.preventDefault();
-        if (cursoEmEdicao) {
+        if (cursoEmEdicao){
             const cursoAtualizado = {
-                id: cursoEmEdicao.id,
-                nome: DadosCurso.nomeCurso,
-                periodo: DadosCurso.periodo
-            };
-            console.log("Alteração em formato JSON :\n", JSON.stringify(cursoAtualizado, null, 2));
-            aoAtualizar(cursoAtualizado)
-
-        } else {
-            const cursoNovo = {
-                id: "",
-                nome: DadosCurso.nomeCurso,
-                periodo: DadosCurso.periodo
-            };
-            console.log("Inclusão em formato JSON :\n", JSON.stringify(cursoNovo, null, 2));
+                id:cursoEmEdicao.id,
+                nome:DadosCurso.nomecurso,
+                periodo:DadosCurso.periodo
+            }
+            console.log("Alteraçãoo em formato JSON:\n", JSON.stringify(cursoAtualizado,null,2));
+            aoAtualizar(cursoAtualizado);
+        }else{
+           const cursoNovo = {
+                id:"",
+                nome: DadosCurso.nomecurso,
+                periodo: DadosCurso.periodo,
+            }
+            console.log("inclusao em formato JSON:\n", JSON.stringify(cursoNovo,null,2));
             aoAdicionar(cursoNovo)
-
         }
-        setDadosCurso({ nomeCurso: "", periodo: "" })
-
     }
 
     return (
         <>
             <Container>
-                <section className={styles.formularioContainer}>
-                    <h2 className={styles.titulo}>
-                        {cursoEmEdicao ? 'Editar Curso' : 'Cadastrar Novo Curso'}
+                <section className={styles.formularioContainer}>  
+                    <h2 className = {styles.titulo}>
+                        Cadastrar curso
+                        {cursoEmEdicao? 'Editar Curso': 'Alterar Curso'} 
                     </h2>
-                    <form onSubmit={cadastrarCurso}>
-                        <div className={styles.pularLinha}>
-                            <label htmlFor="nomeCurso" className={styles.label}>Nome do curso</label>
-                            <InputPadrao
-                                type="text"
-                                id="nomeCurso"
-                                name="nomeCurso"
-                                placeholder="Ex: Desenvolvimento de Sistemas"
-                                value={DadosCurso.nomeCurso}
-                                onChange={lidarComMudanca}
-                                required
-                            />
-                        </div>
-                        <div className={styles.pularLinha}>
-                            <label htmlFor="periodo" className={styles.label}
-                            >Periodo</label>
-                            <select
-                            className={styles.estiloSelect}
-                            
-                                id="periodo"
-                                name="periodo"
-                                value={DadosCurso.periodo}
-                                onChange={lidarComMudanca}
-                                required
-                            >
-                                <option value="">Selecione um periodo</option>
-                                <option value="matutino">Matutino</option>
-                                <option value="vespertino">Vespertino</option>
-                                <option value="noturno">Noturno</option>
-                                <option value="integral">Integral</option>
-                            </select>
-                            
-                        </div>
-                        <div className={styles.alinharBotao}>
-                            <BotaoPadrao type='submit'>
-                                {cursoEmEdicao? 'Salvar Alteração':'Inserir Curso'}
-                            </BotaoPadrao>
-                        </div>
-                    </form>
-                </section>
+                    <form //onSubmit = {cadastrarCurso}
+                    >Olá do form</form>
+                    <div className={styles.pularLinha}>
+                        <label htmlFor='nomeCurso' className={styles.label}>Nome curso:</label>
+                        <InputPadrao
+                        type='text'
+                        id='nomecurso'
+                        name='nomecurso'
+                        placeholder='Ex: DS'
+                        value={DadosCurso.nomecurso}
+                        onChange={lidarComMudanca}
+                        required
+                        >
+                        </InputPadrao>
+                    </div>
+                    <div className={styles.pularLinha}>
+                        <label htmlFor="periodo" className={styles.label}>Periodo</label>
+                        <select name="periodo" id="periodo"
+                        value={DadosCurso.periodo}
+                        onChange={lidarComMudanca}
+                        required
+                        className={styles.estiloSelect}
+                        >
+                            <option value="">Selecione o periodo</option>
+                            <option value="Matutino">Matutino</option>
+                            <option value="Vespertino">Vespertino</option>
+                            <option value="Noturno">Noturno</option>
+                            <option value="Integral">Integral</option>
 
+                        </select>
+                    </div>
+                    <div className={styles.alinharBotao}>
+                        <BotaoPadrao type='submit'>
+                            {/* {cursoEmEdicao? 'Salva alteração': 'Inserir curso'} */}
+                            Botao teste
+                        </BotaoPadrao>
+                    </div>
+                    </section>
             </Container>
         </>
     )
